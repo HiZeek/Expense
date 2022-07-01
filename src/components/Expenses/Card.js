aadddffe6a3ed552a9d2927dd7bd5057f125a250
Isaac Idibia <idibiaisaac3@gmail.com>
2022-07-01 23:17:19 +0100

    date

---
+++ src/components/Expenses/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../../style/Card.css";
+
+const Card = (props) => {
+
+    const classes = 'card ' + props.className;
+
+  return (
+    <div className={classes}>
+        {props.children}
+    </div>
+  )
+}
+
+export default Card;
--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../style/ExpenseDate.css";
+import "../../style/ExpenseDate.css";
 
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
